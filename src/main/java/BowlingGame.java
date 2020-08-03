5889d00e57cbc56b3927dc901be5891a1dff6ca2
chenzdido <chenzdido@gmail.com>
2020-08-03 21:20:26 +0800

    [zhe] feat: return socre when in one fram is not hit ten pin.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,22 @@
 public class BowlingGame {
 
     public int score(int[] pin_score) {
-        return 199;
+        int score=0;
+        int length=pin_score.length;
+        int frame_num=0;
+        for(int i=0;i<length&&frame_num<10;i++,frame_num++){
+            if(pin_score[i]==10){
+                score+= 10 + pin_score[i + 1]+pin_score[i+2];
+            }else{
+                if(pin_score[i]+pin_score[i+1]==10){
+                    score+=10+pin_score[i+2];
+
+                }else{
+                    score+=pin_score[i]+pin_score[i+1];
+                }
+                i++;
+            }
+        }
+        return score;
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -6,7 +6,14 @@ public class BowlingGameTest {
     void should_return_score_when_tenth_frame_first_throw_hit_is_two_more_throw(){
         BowlingGame bowlingGame=new BowlingGame();
         int score=bowlingGame.score(new int[]{10, 10, 10, 10,10,10,10,10,10,10,4,5});
-        assertEquals(199,score);
+        assertEquals(283,score);
+    }
+
+    @Test
+    void should_return_score_when_in_one_frame_is_not_hit_ten_pin(){
+        BowlingGame bowlingGame=new BowlingGame();
+        int score=bowlingGame.score(new int[]{8, 1, 3, 4,10,5,4,10,10,10,10,10,10,4,5});
+        assertEquals(207,score);
     }
 
 }
