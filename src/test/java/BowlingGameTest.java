673d08ccd8ee08947170b8e6da4d2370cb860d6c
chenzdido <chenzdido@gmail.com>
2020-08-03 21:23:14 +0800

    [zhe] feat:return socre when in one fram is spare hit ten pin.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -16,4 +16,11 @@ public class BowlingGameTest {
         assertEquals(207,score);
     }
 
+    @Test
+    void should_return_score_when_in_one_frame_is_spare_hit_ten_pin(){
+        BowlingGame bowlingGame=new BowlingGame();
+        int score=bowlingGame.score(new int[]{8, 1, 3, 4,10,5,4,6,4,10,7,3,10,10,10,4,5});
+        assertEquals(177,score);
+    }
+
 }
