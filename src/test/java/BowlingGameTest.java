29af1df63112de1b1df05894f0e6098b41c8018b
chenzdido <chenzdido@gmail.com>
2020-08-03 21:32:57 +0800

    [zhe] feat:5th test return socre when tenth frame is not hit ten pin.

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
     @Test
-    void should_return_score_when_tenth_frame_first_throw_hit_is_two_more_throw(){
+    void should_return_score_when_tenth_frame_is_strike(){
         BowlingGame bowlingGame=new BowlingGame();
         int score=bowlingGame.score(new int[]{10, 10, 10, 10,10,10,10,10,10,10,4,5});
         assertEquals(283,score);
@@ -17,17 +17,24 @@ public class BowlingGameTest {
     }
 
     @Test
-    void should_return_score_when_in_one_frame_is_spare_hit_ten_pin(){
+    void should_return_score_when_in_one_frame_except_tenth_is_spare(){
         BowlingGame bowlingGame=new BowlingGame();
         int score=bowlingGame.score(new int[]{8, 1, 3, 4,10,5,4,6,4,10,7,3,10,10,10,4,5});
         assertEquals(177,score);
     }
 
     @Test
-    void should_return_socre_when_thenth_frame_first_spare_hit_ten_pin(){
+    void should_return_score_when_tenth_frame_is_spare(){
         BowlingGame bowlingGame=new BowlingGame();
         int score=bowlingGame.score(new int[]{8, 1, 3, 4,10,5,4,6,4,10,7,3,10,10,6,4,5});
         assertEquals(165,score);
+    }
+
+    @Test
+    void should_return_score_when_tenth_frame_is_not_hit_ten_pin(){
+        BowlingGame bowlingGame=new BowlingGame();
+        int score=bowlingGame.score(new int[]{8, 1, 3, 4,10,5,4,6,4,10,7,3,10,10,3,4});
+        assertEquals(151,score);
     }
 
 }
